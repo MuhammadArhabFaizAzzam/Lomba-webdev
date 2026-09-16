49b0236522c8ccfb5e2f80950033c1f6fe762ab2
M. Arkandi Syahputra Santosa <138979992+lemaopisang@users.noreply.github.com>
2026-09-16 15:18:26 +0700

    feat: add GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Lomba-webdev',
+  assetPrefix: '/Lomba-webdev',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
